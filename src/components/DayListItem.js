63f498ee35d084f94e65dbe757d057e48b6bdf76
Umang Patel <umangpatel1991@gmail.com>
2021-10-26 17:28:08 -0400

    modify spots remaining text message

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,15 +3,27 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+const formatSpots = (spots) => {
+  if (spots <= 0) {
+    return "no spots";
+  } else if (spots === 1) {
+    return spots + " spot";
+  } else {
+    return spots + " spots";
+  }
+};
+
 export default function DayListItem(props) {
   let dayClass = classNames('day-list__item', {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
   });
+
+  const spotsText = formatSpots(props.spots);
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsText} remaining</h3>
     </li>
   );
 }
