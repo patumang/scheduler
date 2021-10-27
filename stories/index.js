8b756a5fda8bcb59c042252a68fd6b83983df722
Umang Patel <umangpatel1991@gmail.com>
2021-10-27 17:44:41 -0400

    add test story for Empty component

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 /* Button story start */
 storiesOf("Button", module)
@@ -154,4 +155,5 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
 /* Appointment story end */
