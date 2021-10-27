409db3c5e90d039051a0ff39e6666ea46ab94805
Umang Patel <umangpatel1991@gmail.com>
2021-10-27 17:13:55 -0400

    create and add styles to Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      {props.time ? <p>Appointment at {props.time}</p> : <p>No Appointments</p>}
+    </article>
+  );
+};
