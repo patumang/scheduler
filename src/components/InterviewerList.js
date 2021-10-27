40d71a2bb994de7cf4460ed34bdb2e6beabb40f7
Umang Patel <umangpatel1991@gmail.com>
2021-10-27 15:10:15 -0400

    create and style InterviewerList component to render all InterviewerListItems

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {
+          interviewers.map((interv) => (
+            <InterviewerListItem
+              key={interv.id}
+              id={interv.id}
+              name={interv.name}
+              avatar={interv.avatar}
+              selected={interv.id === interviewer}
+              setInterviewer={setInterviewer}
+            />
+          ))
+        }
+      </ul>
+    </section>
+  );
+}
