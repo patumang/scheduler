11151a75e62ba03e9119f30cbba7d4dc872495f6
Umang Patel <umangpatel1991@gmail.com>
2021-10-27 15:11:50 -0400

    remove commented code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,21 +3,9 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const { days, day, setDay } = props;
-  /* const parsedDayList = days.map((d) => {
-    return (
-      <DayListItem 
-        key={d.id} 
-        name={d.name}
-        spots={d.spots}
-        selected= {d.name === day}
-        onClick={() => setDay(d.name)} 
-      />
-    );
-  }); */
   console.log(setDay);
   return (
     <ul>
-      {/* {parsedDayList} */}
       {
         days.map((d) => (
             <DayListItem
