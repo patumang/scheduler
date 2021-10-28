43fdf382f70f1c957fe87e90447ae27b2b651fa8
Umang Patel <umangpatel1991@gmail.com>
2021-10-28 08:21:41 -0400

    render Header, Empty and Show components for an Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "./styles.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {props.time ? <p>Appointment at {props.time}</p> : <p>No Appointments</p>}
+      {props.time ? <Header time={props.time} /> : <p>No Appointments</p>}
+      {props.interview 
+        ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />
+        : <Empty onAdd={props.onAdd} />
+      }
     </article>
   );
 };
