2409c3baefb85c76d84923ae6f0c7a80b31c110c
Umang Patel <umangpatel1991@gmail.com>
2021-10-27 21:24:49 -0400

    add test story for Status component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 /* Button story start */
 storiesOf("Button", module)
@@ -171,4 +172,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")} />
   )
+  .add("Status", () => <Status message="Deleting" />)
 /* Appointment story end */
