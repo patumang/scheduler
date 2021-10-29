5bc2d6e019c36299f4a09d2984452fa83060ff5b
Umang Patel <umangpatel1991@gmail.com>
2021-10-29 16:47:36 -0400

    get  interviewer details by calling api endpoints and pass it to component

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,14 +4,15 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getInterview, getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   //const dailyAppointments = [];
@@ -23,13 +24,26 @@ export default function Application(props) {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-      console.log(all);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -54,11 +68,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {
-          dailyAppointments.map((appointment) => (
-            <Appointment key={appointment.id} {...appointment} />
-          ))
-        }
+        { schedule }
         <Appointment key="last" time="5pm" />
       </section>
     </main>
