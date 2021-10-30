8ecfb6243876d3fb32defd0c802ebe6f653f7ebe
Umang Patel <umangpatel1991@gmail.com>
2021-10-29 22:37:25 -0400

    add initial, transit and back mode for various constants defined in component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,38 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       {props.time ? <Header time={props.time} /> : <p>No Appointments</p>}
-      {props.interview 
-        ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />
-        : <Empty onAdd={props.onAdd} />
-      }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => transition(SAVING)}
+          onCancel={() => back()} />
+      )}
+      
     </article>
   );
 };
