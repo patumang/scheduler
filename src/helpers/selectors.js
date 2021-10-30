5e2e0a434249f6ded3185626678de98449cb3d84
Umang Patel <umangpatel1991@gmail.com>
2021-10-29 23:07:34 -0400

    create function getInterviewersForDay to return interviewers for input day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,6 +13,20 @@ export function getAppointmentsForDay(state, day) {
   return appointments;
 };
 
+export function getInterviewersForDay(state, day) {
+  const interviewers = [];
+  for (const currDay of state.days) {
+    if (currDay.name === day) {
+      for (const interviewer of currDay.interviewers) {
+        interviewers.push(state.interviewers[interviewer]);
+      }
+    }
+  }
+
+  //... returns an array of interviewers for that day
+  return interviewers;
+};
+
 export function getInterview(state, interview) {
   let parsedInterview = null;
 
