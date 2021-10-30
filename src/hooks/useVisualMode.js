724d4443d9b1de8629fbe240f57c13a67d181806
Umang Patel <umangpatel1991@gmail.com>
2021-10-29 21:52:49 -0400

    add transition with replace and add test for it

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,11 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
-    setHistory([...history, newMode]);
+    if(!replace) {
+      setHistory([...history, newMode]);
+    }
   };
   const back = () => {
     history.pop();
