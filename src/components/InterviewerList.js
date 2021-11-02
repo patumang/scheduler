31859bb51afd2510d05a611336b0ee74b63aba1c
Umang Patel <umangpatel1991@gmail.com>
2021-11-02 17:12:54 -0400

    define type and isRequired for interviewers props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const { interviewers, value, onChange } = props;
   return(
     <section className="interviewers">
@@ -24,3 +25,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
