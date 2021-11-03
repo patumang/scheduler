30bdb23a52a3d2520dddf2fa5780900fd87c6fa5
Umang Patel <umangpatel1991@gmail.com>
2021-11-03 19:52:37 -0400

    add test cases for edit  interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -4,6 +4,7 @@ describe("Appointments", () => {
     cy.visit("/");
     cy.contains("Monday");
   });
+
   it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
@@ -11,6 +12,15 @@ describe("Appointments", () => {
     cy.contains("Save").click();
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
 });
