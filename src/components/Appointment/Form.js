d48441382480e1062edeef08295925dbeb79d0a4
Umang Patel <umangpatel1991@gmail.com>
2021-11-03 13:42:42 -0400

    Add form validation for student name cannot be blank

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,15 +6,27 @@ import InterviewerList from "../InterviewerList";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
     setInterviewer(null);
+    setError("");
   }
 
   const cancel = () => {
     reset();
     props.onCancel();
+  }
+
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+
+    props.onSave(student, interviewer);
   }
 
   return(
@@ -28,7 +40,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -39,7 +53,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
